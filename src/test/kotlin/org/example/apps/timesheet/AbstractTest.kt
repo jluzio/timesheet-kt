5ef9aa593791d8ea7254f47569e864271fc00688
João Luzio <joao.luzio@gmail.com>
2019-07-30 15:59:33 +0100

    Added libraries and infrastructure testing

--- src/test/kotlin/org/example/apps/timesheet/AbstractTest.kt
+++ src/test/kotlin/org/example/apps/timesheet/AbstractTest.kt
@@ -1,5 +1,6 @@
 package org.example.apps.timesheet
 
+import org.apache.logging.log4j.LogManager
 import org.junit.runner.RunWith
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.junit4.SpringRunner
@@ -7,4 +8,6 @@ import org.springframework.test.context.junit4.SpringRunner
 @RunWith(SpringRunner::class)
 @SpringBootTest
 abstract class AbstractTest {
+    val log = LogManager.getLogger(javaClass)
+
 }
--- src/test/kotlin/org/example/apps/timesheet/test/InfrastructureTest.kt
+++ src/test/kotlin/org/example/apps/timesheet/test/InfrastructureTest.kt
@@ -1,12 +1,10 @@
 package org.example.apps.timesheet.test
 
-import org.apache.logging.log4j.LogManager
 import org.example.apps.timesheet.AbstractTest
 import org.junit.Test
 import javax.inject.Inject
 
 class InfrastructureTest: AbstractTest() {
-    val log = LogManager.getLogger(javaClass)
     @Inject
     lateinit var fooService: FooService
 
