32a9b4aee9ed156d73deb63fc096883cc16b9faf
João Luzio <joao.luzio@gmail.com>
2019-08-01 19:51:43 +0100

    Entries and Processors (WIP)

---
+++ src/main/kotlin/org/example/apps/timesheet/processors/TimesheetProcessor.kt
@@ -0,0 +1,13 @@
+package org.example.apps.timesheet.processors
+
+import org.example.apps.timesheet.config.ProcessConfig
+import javax.inject.Named
+
+@Named
+class TimesheetProcessor {
+
+
+    fun process(processConfig: ProcessConfig) {
+
+    }
+}
