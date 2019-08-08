3760d5a19e431c6189fbbadf9ec7460f01299479
João Luzio <joao.luzio@gmail.com>
2019-08-08 20:30:23 +0100

    Adding tests

--- src/test/kotlin/org/example/apps/timesheet/test/models.kt
+++ src/test/kotlin/org/example/apps/timesheet/test/models.kt
@@ -1,7 +1,16 @@
 package org.example.apps.timesheet.test
 
 import java.time.LocalDate
+import javax.xml.bind.annotation.XmlAccessType
+import javax.xml.bind.annotation.XmlAccessorType
 import javax.xml.bind.annotation.XmlRootElement
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter
 
 @XmlRootElement
-data class Person(var firstName: String = "", var surname: String = "", var birthDate: LocalDate? = null)
+//@XmlAccessorType(XmlAccessType.FIELD)
+data class Person(
+        var firstName: String = "",
+        var surname: String = "",
+        @get:XmlJavaTypeAdapter(io.github.threetenjaxb.core.LocalDateXmlAdapter::class)
+        var birthDate: LocalDate? = null
+)
--- src/test/kotlin/org/example/apps/timesheet/test/package-info.java
+++
@@ -1,5 +0,0 @@
-@javax.xml.bind.annotation.adapters.XmlJavaTypeAdapters({
-        @javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter(type = java.time.LocalDate.class, value = io.github.threetenjaxb.core.LocalDateXmlAdapter.class),
-        @javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter(type = java.time.LocalDateTime.class, value = io.github.threetenjaxb.core.LocalDateTimeXmlAdapter.class),
-})
-package org.example.apps.timesheet.test;
